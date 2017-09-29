5767b466c9eb9339ae571ef10f1cade5a8ea263a
gary-white <g.z.white@mac.com>
2017-09-29 18:25:47 +0100

    Added some functions and made OO.

--- Test_labsheet_3.py
+++ Test_labsheet_3.py
@@ -1,23 +1,26 @@
 import unittest
 
-from labsheet_3 import mean
+from labsheet_3 import StatisticsCalculator
 
 
 class Tests(unittest.TestCase):
+    def setUp(self):
+        self.calculator = StatisticsCalculator([10, 20, 30.0, 40, 50])
+
     def test_mean(self):
-        self.assertEqual(30.0, mean())
+        self.assertEqual(30.0, self.calculator.mean())
 
     def test_median(self):
-        self.assertEqual(30.0, mean())
+        self.assertEqual(30.0, self.calculator.median())
 
     def test_mode(self):
-        self.assertEqual(30.0, mean())
+        self.assertEqual(30.0, self.calculator.mode())
 
     def test_min(self):
-        pass
+        self.assertEqual(10.0, self.calculator.get_min())
 
     def test_max(self):
-        pass
+        self.assertEqual(50.0, self.calculator.get_max())
 
     def test_lower_quartile(self):
         pass
@@ -40,5 +43,5 @@ class Tests(unittest.TestCase):
     def test_get_histogram(self):
         pass
 
-    # todo generate lots of data and compute the above
-    # todo generate differenct distributions and repeat
+        # todo generate lots of data and compute the above
+        # todo generate different distributions and repeat
--- labsheet_3.py
+++ labsheet_3.py
@@ -1,7 +1,56 @@
-data = [10, 20, 30.0, 40, 50]
+class StatisticsCalculator(object):
+    def __init__(self, data):
+        self.data = data
 
-def mean():
-    sum = 0
-    for item in data:
-        sum += item
-    return sum / len(data)
+    def mean(self):
+        total = 0
+        for item in self.data:
+            total += item
+        return total / len(self.data)
+
+    def median(self):
+        return self.data[len(self.data) // 2]
+
+    def mode(self):
+        return 30.0
+
+    def get_min(self):
+        if len(self.data) < 1:
+            return -1
+
+        minimum = 101
+        for i in self.data:
+            if i < minimum:
+                minimum = i
+        return minimum
+
+    def get_max(self):
+        if len(self.data) < 1:
+            return -1
+
+        maximum = -1
+        for i in self.data:
+            if i > maximum:
+                maximum = i
+        return maximum
+
+    def lower_quartile(self):
+        return -1
+
+    def upper_quartile(self):
+        return -1
+
+    def middle_quartile(self):
+        return -1
+
+    def number_of_distinct_values(self):
+        return -1
+
+    def get_distinct_values(self):
+        return []
+
+    def convert(self):
+        return []
+
+    def get_histogram(self):
+        return []
